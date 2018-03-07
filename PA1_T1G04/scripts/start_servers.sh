5182d72a49a07a0444120aa3a8877b1d727dcea0
Tiago Faria <tiaghpf@ua.pt>
2018-03-07 22:46:02 +0000

    Add KAKFA and script to run servers

---
+++ PA1_T1G04/scripts/start_servers.sh
@@ -0,0 +1,2 @@
+../kafka_2.11-1.0.1/bin/zookeeper-server-start.sh ../kafka_2.11-1.0.1/config/zookeeper.properties |
+../kafka_2.11-1.0.1/bin/kafka-server-start.sh ../kafka_2.11-1.0.1/config/server.properties
